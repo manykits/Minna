16ace32d1621dca4a648862d55a657fa725df77b
manyxu <manyxu@foxmail.com>
2025-01-31 11:48:23 +0800

    update db.js

--- common/db.js
+++ common/db.js
@@ -1,7 +1,7 @@
 // db.js
 
 var mongoose = require('mongoose');
-var url = "mongodb://mthing:mt123@localhost:27017/mthing?authSource=admin"
+var url = "mongodb://mthing:minna1314@localhost:27017/mthing?authSource=admin"
 mongoose.connect(url,{ useNewUrlParser: true , useUnifiedTopology: true}, function(err, db) {
     if (err){
         console.log("mongodb connect failed!");
